6a6092a87f7201aa5a789b25db2ebb9d17e5106a
Paradiseduo <>
2020-03-27 10:37:12 +0800

    修复崩溃问题

--- tcping/main.swift
+++ tcping/main.swift
@@ -17,20 +17,22 @@ let queue = DispatchQueue(label: "tcping")
 Tcping().staticMode()
 
 group.notify(queue: DispatchQueue.main) {
-    let successArr = sockets.filter { (p) -> Bool in
-        return p.speed != TimeInterval.infinity
-    }
-    let max = successArr.max { (a, b) -> Bool in
-        return a.speed < b.speed
-    }?.speed
-    let min = successArr.min { (a, b) -> Bool in
-        return a.speed < b.speed
-    }?.speed
-    if successArr.count == 0 {
-        ConsoleIO().printResult(isFinish: true, detail: sockets[0], count: UInt(sockets.count), lossCount: UInt(sockets.count), min: NSNumber(value: Double.infinity), max: NSNumber(value: Double.infinity), avge: NSNumber(value: Double.infinity))
-    } else {
-        let avge = successArr.reduce(0.0) { (result: Double, p: tcping) -> Double in return result+p.speed}/Double(successArr.count)
-        ConsoleIO().printResult(isFinish: true, detail: sockets[0], count: UInt(sockets.count), lossCount: UInt(sockets.count-successArr.count), min: NSNumber(value: min!), max: NSNumber(value: max!), avge: NSNumber(value: avge))
+    if sockets.count > 0 {
+        let successArr = sockets.filter { (p) -> Bool in
+            return p.speed != TimeInterval.infinity
+        }
+        let max = successArr.max { (a, b) -> Bool in
+            return a.speed < b.speed
+        }?.speed
+        let min = successArr.min { (a, b) -> Bool in
+            return a.speed < b.speed
+        }?.speed
+        if successArr.count == 0 {
+            ConsoleIO().printResult(isFinish: true, detail: sockets[0], count: UInt(sockets.count), lossCount: UInt(sockets.count), min: NSNumber(value: Double.infinity), max: NSNumber(value: Double.infinity), avge: NSNumber(value: Double.infinity))
+        } else {
+            let avge = successArr.reduce(0.0) { (result: Double, p: tcping) -> Double in return result+p.speed}/Double(successArr.count)
+            ConsoleIO().printResult(isFinish: true, detail: sockets[0], count: UInt(sockets.count), lossCount: UInt(sockets.count-successArr.count), min: NSNumber(value: min!), max: NSNumber(value: max!), avge: NSNumber(value: avge))
+        }
     }
     NotificationCenter.default.post(name: NSNotification.Name("stop"), object: nil)
 }
